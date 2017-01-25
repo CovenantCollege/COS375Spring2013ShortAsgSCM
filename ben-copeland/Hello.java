e1fc8f2a9c38ab536ef8c745c95537676f23d87a
ben-copeland <ben.copeland@covenant.edu>
2017-01-25 13:39:41 -0500

    Version 1.0

--- ben-copeland/Hello.java
+++ ben-copeland/Hello.java
@@ -1,7 +1,8 @@
+// changed from saying "Hello World" to saying "Hello" followed by my name
 
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Ben");
 	}
 }
