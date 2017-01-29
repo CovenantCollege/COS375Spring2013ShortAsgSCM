b73dcb3f7e01cc0348aa6992b2e4f4c23ccb1d63
Spencer Dent <spencer.dent@covenant.edu>
2017-01-28 20:22:28 -0500

    Version 1.0

--- masterned/Hello.java
+++ masterned/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String args[]) {
 		String name = "Spencer"
-		System.out.println("Hello, World!");
-		//System.out.println("Hello, " + name + "!");
+		//System.out.println("Hello, World!");
+		System.out.println("Hello, " + name + "!");
 	}
 }
